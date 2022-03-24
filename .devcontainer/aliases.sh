f91a959338d2f4d7e64079adc2c245e3491e2978
Sean Killeen <SeanKilleen@gmail.com>
2022-03-23 22:05:26 -0500

    create aliases script

---
+++ .devcontainer/aliases.sh
@@ -0,0 +1,5 @@
+# We get the first workspace, because there should only be one
+# This is so that when someone updates their devcontainer name, it doesn't get screwed up
+first_workspace=$(cd /workspaces && ls | head -1)
+
+alias localdev= cd $first_workspace/src bundle install && bundle exec jekyll serve --future"
