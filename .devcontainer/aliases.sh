4d96c8420b0852615dcf46c60aef802ae9cdbc47
Sean Killeen <SeanKilleen@gmail.com>
2022-03-24 16:26:32 -0400

    remove spaces

--- .devcontainer/aliases.sh
+++ .devcontainer/aliases.sh
@@ -1,10 +1,7 @@
 # We get the first workspace, because there should only be one
 # This is so that when someone updates their devcontainer name, it doesn't get screwed up
 first_workspace=$(cd /workspaces && ls | head -1)
-
 alias gotoworkspace="cd /workspaces/$first_workspace/src"
-
 alias localdev="gotoworkspace && bundle install && bundle exec jekyll serve --future"
 alias serve="gotoworkspace && bundle exec jekyll serve --future"
-
 alias spellcheck="cd /blog-in-a-box_tools && npx cspell --config \"/workspaces/$first_workspace/cSpell.json\" \"/workspaces/$first_workspace/src/**/*.md\""
