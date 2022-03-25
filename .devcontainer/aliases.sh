e28dc675b0902fe39859c7b51f3030674b2f2cf1
Sean Killeen <SeanKilleen@gmail.com>
2022-03-24 20:55:51 -0400

    fix  "issue"
    Also, need to deal with line endings (the real issue)

--- .devcontainer/aliases.sh
+++ .devcontainer/aliases.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 # We get the first workspace, because there should only be one
 # This is so that when someone updates their devcontainer name, it doesn't get screwed up
-first_workspace=$(cd /workspaces && ls | head -1)
+first_workspace="$(cd /workspaces && ls | head -1)"
 alias gotoworkspace="cd /workspaces/$first_workspace/src"
 alias localdev="gotoworkspace && bundle install && bundle exec jekyll serve --future"
 alias serve="gotoworkspace && bundle exec jekyll serve --future"
